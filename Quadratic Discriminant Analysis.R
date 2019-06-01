98f78df184cd07f1e9872aa200bb2a81ff27de31
patrickfzakrzewski <42501097+patrickfzakrzewski@users.noreply.github.com>
2019-06-01 11:57:31 -0400

    Update Quadratic Discriminant Analysis.R

--- Quadratic Discriminant Analysis.R
+++ Quadratic Discriminant Analysis.R
@@ -1,1 +1,6 @@
-
+#Continuatino of LDA, some variables are declared there
+qda.fit=qda(Direction∼Lag1+Lag2 ,data=Smarket ,subset=train)
+qda.fit
+qda.class=predict (qda.fit ,Smarket.2005)$class
+table(qda.class,Direction.2005)
+mean(qda.class==Direction.2005)
