90ee7dffb32e89bb1f7ad4c1ac48420e12f5379a
patrickfzakrzewski <42501097+patrickfzakrzewski@users.noreply.github.com>
2019-06-02 20:01:21 -0400

    Create Bootstrap.R

---
+++ Bootstrap.R
@@ -0,0 +1,25 @@
+##Bootstrap
+##Minimnum risk investment
+
+alpha=function(x,y){ #function to determine optimal investment strategy between two assets so that risk (variance) is minimized
+  vx=var(x)
+  vy=var(y)
+  cxy=cov(x,y)
+  (vy-cxy)/(vx-vy-2*cxy)
+ }
+ 
+alpha(Portfolio$X,Portfolio$Y)
+
+
+alpha.fn=function(data,index){
+  with(data[index,],alpha(X,Y))
+}
+
+alpha.fn(Portfolio,1:100)
+
+set.seed(1)
+alpha.fn(Portfolio,sample(1:100,100,replace=TRUE) #of rows 1 to 100, going to take 100 random samples with replace=true and apply alpha function, gives bootstrap of sample size 1
+
+boot.out=boot(Portfolio,alphafn,R=1000)
+boot.out
+plot(boot.out)
